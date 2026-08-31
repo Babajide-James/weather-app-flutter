e68264abb9ef336b0eedd867d6fbd05664e07432
James Babajide <teestartaiwo121@gmail.com>
2026-08-31 14:45:39 +0100

    Change namespace from 'com.example.news_api' to 'com.skycast.weather'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.news_api"
+    namespace = "com.skycast.weather"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
